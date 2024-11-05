3d47f27a815e29df2dd0b87c5e2a12165a7a8f2e
kbnopro <tuankhanhb00@gmail.com>
2024-11-05 15:51:56 +0700

    feat: additional keymap for oil

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -3,6 +3,9 @@ return {
   ---@module 'oil'
   ---@type oil.SetupOpts
   opts = {},
+  keys = {
+    { "<leader>o", "<cmd>Oil<cr>", desc = "Oil" },
+  },
   -- Optional dependencies
   dependencies = { { "echasnovski/mini.icons", opts = {} } },
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
