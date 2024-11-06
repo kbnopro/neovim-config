1f9ac6af6c45457caec16215491d9696c8dd7b8b
kbnopro <tuankhanhb00@gmail.com>
2024-11-06 12:55:25 +0700

    feat: implement supertab

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,39 +1,20 @@
+-- Load snippets in LuaSnip
+
 return {
   {
     "L3MON4D3/LuaSnip",
-    -- follow latest release.
-    version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
-    -- install jsregexp (optional!).
-    build = "make install_jsregexp",
-    opts = {
-      enable_autosnippets = true,
-      store_selection_keys = "<Tab>",
-      update_events = "TextChanged,TextChangedI",
-    },
-    init = function()
-      require("luasnip.loaders.from_lua").lazy_load({ paths = "~/.config/nvim/LuaSnip/" })
+    config = function(_, opts)
+      require("luasnip").setup(opts)
+      require("luasnip.loaders.from_lua").load({ paths = { "~/.config/nvim/LuaSnip/" } })
+      require("luasnip").config.set_config({
+        enable_autosnippets = true,
+        store_selection_keys = "<Tab>",
+        update_events = { "TextChanged", "TextChangedI" },
+      })
     end,
   },
   {
     "rafamadriz/friendly-snippets",
     enabled = false,
-    config = function()
-      require("luasnip.loaders.from_vscode").lazy_load()
-    end,
-  },
-  {
-    "nvim-cmp",
-  -- stylua: ignore
-    keys = {
-      {
-        "<tab>",
-        function()
-          return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next" or "<tab>"
-        end,
-        expr = true, silent = true, mode = "i",
-      },
-      { "<tab>", function() require("luasnip").jump(1) end, mode = "s" },
-      { "<s-tab>", function() require("luasnip").jump(-1) end, mode = { "i", "s" } },
-    },
   },
 }
